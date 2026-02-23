c8dfe3e8d352503b044abef837983d0cce4ca641
kitamura <>
2026-02-24 06:48:28 +0900

    20260224 tomohisa プロジェクトgithub管理開始

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-50">
+      <h1 className="text-5xl font-black text-blue-600 animate-bounce">
+        北村知久
+      </h1>
+      <p className="mt-4 text-gray-500 font-bold">
+        SCSSもBEMも使わずにスタイルが当たりました。
+      </p>
+    </div>
+  );
+}
+
+export default App;
