333e89b20fcaf4dfea4f335d74a6fc5fa2f13868
kitamura <>
2026-02-24 07:03:25 +0900

    20260224tomohisa test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-blue-50">
       <h1 className="text-5xl font-black text-blue-600 animate-bounce">
-        北村知久
+        北村知久!!
       </h1>
       <p className="mt-4 text-gray-500 font-bold">
         SCSSもBEMも使わずにスタイルが当たりました。
