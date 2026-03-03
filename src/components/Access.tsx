12cd8e9b1fb647fad4fbc682af8054f2de40ee05
tomohisa <tomohisa520519@gmail.com>
2026-03-04 07:22:36 +0900

    202603034 tomohisa

--- src/components/Access.tsx
+++ src/components/Access.tsx
@@ -3,14 +3,14 @@
 
 export default function Access() {
   return (
-    <section className="border border-blue-500 pt-20">
+    <section className="border border-blue-500 pt-20 text-white">
       <div className="max-w-[1156px] mx-auto border border-red-500">
         <h1 className="text-[40px] font-bold text-[#3B5CA9] text-center mb-10">交通アクセス</h1>
-        <div className="selectBlock flex justify-between max-w-[400px] mx-auto">
-          <div className="osaka">
+        <div className="selectBlock flex justify-between max-w-[400px] mx-auto border border-blue-500">
+          <div className="osaka border bg-[#3B5CA9] w-[50%] text-center ">
             大阪オフィス
           </div>
-          <div className="tokyo">
+          <div className="tokyo border border-red-500 w-[50%] text-center">
             東京オフィス
           </div>
         </div>
